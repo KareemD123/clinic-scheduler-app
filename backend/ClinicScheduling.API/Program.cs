a98701d863ca1d24279e514432e4847db8ff408f
KareemD123 <kareemdraz.careers@gmail.com>
2025-09-29 23:06:55 -0400

    minor CORS fix

--- backend/ClinicScheduling.API/Program.cs
+++ backend/ClinicScheduling.API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200", "http://localhost:4201")
+            policy.WithOrigins("http://localhost:4200", "http://localhost:4201", "http://localhost:5000")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
