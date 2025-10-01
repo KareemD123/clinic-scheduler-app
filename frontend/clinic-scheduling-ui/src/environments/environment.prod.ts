fdd6669201cfbc4655475eefa489d5234fedd7ba
KareemD123 <kareemdraz.careers@gmail.com>
2025-10-01 13:29:32 -0400

    Update frontend environment variables and add Static Web Apps config

--- frontend/clinic-scheduling-ui/src/environments/environment.prod.ts
+++ frontend/clinic-scheduling-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'https://clinic-scheduler-cpcfhfeha8hpb6gs.canadacentral-01.azurewebsites.net/api'
 };
--- frontend/clinic-scheduling-ui/src/environments/environment.ts
+++ frontend/clinic-scheduling-ui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5188/api'
+  apiUrl: 'http://localhost:5000/api'
 };
 
 /*
